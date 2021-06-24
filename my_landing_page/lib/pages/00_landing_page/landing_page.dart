be62329e2d53dfb2a569a2adf53e0f2a58be9d7a
Firdous <fn24x7@gmail.com>
2021-06-24 19:10:49 -0400

    Removing expanded widget from landing page build

--- my_landing_page/lib/pages/00_landing_page/landing_page.dart
+++ my_landing_page/lib/pages/00_landing_page/landing_page.dart
@@ -1,58 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:my_landing_page/pages/00_landing_page/hi_intro.dart';
+// import 'package:my_landing_page/pages/00_landing_page/hi_intro.dart';
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final double width = MediaQuery.of(context).size.width;
+    final double width = MediaQuery.of(context).size.width;
     final double height = MediaQuery.of(context).size.height;
     return Material(
       child: Stack(
         children: [
-          Expanded(
-            child: Container(
-              color: Colors.amber,
+          Container(
+            width: width,
+            height: height,
+            color: Colors.amber,
+          ),
+          Container(
+            width: width,
+            height: height,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/BG_Landing.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          Expanded(
-            child: Image.asset(
-              "assets/BG_Landing.jpg",
-              // width: width,
-              // height: height,
-              // alignment: Alignment(-width * 0.5, 0),
-              // fit: BoxFit.cover,
-            ),
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Expanded(
-                // alignment: Alignment.centerLeft,
-                flex: 4,
-                child: HiIntro(),
-              ),
-              Expanded(
-                flex: 5,
-                child: GestureDetector(
-                  child: CustomPaint(
-                    size: Size(200, height),
-                    painter: SliderPainter(),
-                  ),
-                  // Image.asset(
-                  //   "assets/WhiteButtonSlider.png",
-                  // alignment: Alignment.centerRight,
-                  // // width: width * 2,
-                  // height: height,
-                  //   fit: BoxFit.fill,
-                  // ),
-                  onTap: () {
-                    debugPrint("GO TO WORK PAGE!!");
-                  },
-                ),
-              ),
-            ],
-          ),
+          // Row(
+          //   mainAxisSize: MainAxisSize.max,
+          //   mainAxisAlignment: MainAxisAlignment.end,
+          //   children: [
+          //     Expanded(
+          //       flex: 4,
+          //       child: HiIntro(),
+          //     ),
+          //     Expanded(
+          //       flex: 5,
+          //       child: GestureDetector(
+          //         child: CustomPaint(
+          //           size: Size(200, height),
+          //           painter: SliderPainter(),
+          //         ),
+          //         onTap: () {
+          //           debugPrint("GO TO WORK PAGE!!");
+          //         },
+          //       ),
+          //     ),
+          //   ],
+          // ),
         ],
       ),
     );
